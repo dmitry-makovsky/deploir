ddef61b5dddfc643abbbfa6b4144483f856410e3
Makovsky Dmitry <dima@makovsky.me>
2025-04-22 12:21:12 +0300

    feat: add installation script for Docker on Debian-based systems

---
+++ install.sh
@@ -0,0 +1,28 @@
+# Check is not debian based - exit
+if [ ! -f /etc/debian_version ]; then
+    echo "This script is only for Debian-based systems."
+    exit 1
+fi
+# Check if the script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root. Please use sudo."
+    exit 1
+fi
+
+# Is docker installed? Install if not
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Installing Docker..."
+    apt-get update
+    apt-get install -y ca-certificates curl
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update
+    apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+else
+    echo "Docker is already installed."
+fi
